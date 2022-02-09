572b8786c054e8bd742a3b8af6c36e04a03b6060
BitisG <59557015+BitisG@users.noreply.github.com>
2022-02-09 18:03:30 +0100

    helloWorld

---
+++ src/main/java/dtu/project/log4jboot2root/Controller.java
@@ -0,0 +1,12 @@
+package dtu.project.log4jboot2root;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String index() {
+        return ("Hello World!");
+    }
+}
