317785748b9adeb64d2f6034ed0d5fef3abe069a
BitisG <59557015+BitisG@users.noreply.github.com>
2022-02-09 19:21:09 +0100

    Form submission
    It doesn't work atm for some reason. I hate html

--- src/main/java/dtu/project/log4jboot2root/Controller.java
+++ src/main/java/dtu/project/log4jboot2root/Controller.java
@@ -3,12 +3,11 @@ package dtu.project.log4jboot2root;
 import org.apache.tomcat.util.file.ConfigurationSource;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.ui.Model;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -29,6 +28,18 @@ public class Controller {
     public String robots(){
         Resource resource = loader.getResource("classpath:static/robots.txt");
         return asString(resource);
+    }
+
+    @GetMapping("/16144cf950518a312e26b9827d91449d166ed6e38e7cc569a9f3c559")
+    public String ticketForm(Model model){
+        model.addAttribute("ticket", new Ticket());
+        return "ticket";
+    }
+
+    @PostMapping("16144cf950518a312e26b9827d91449d166ed6e38e7cc569a9f3c559")
+    public String ticketSubmit(@ModelAttribute Ticket ticket, Model model){
+        model.addAttribute("ticket", ticket);
+        return "result";
     }
 
     public static String asString(Resource resource) {
---
+++ src/main/java/dtu/project/log4jboot2root/Ticket.java
@@ -0,0 +1,22 @@
+package dtu.project.log4jboot2root;
+
+public class Ticket {
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
