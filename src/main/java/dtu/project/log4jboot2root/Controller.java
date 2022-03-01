53d65307601727dad1a35eae5b219f9a9b2b83d5
Holmness <59558117+Holmness@users.noreply.github.com>
2022-03-01 21:43:05 +0100

    Added functional SQL lookup
    Hopefully

--- src/main/java/dtu/project/log4jboot2root/Controller.java
+++ src/main/java/dtu/project/log4jboot2root/Controller.java
@@ -2,24 +2,46 @@ package dtu.project.log4jboot2root;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.ui.Model;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UncheckedIOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Base64;
+import java.util.List;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+
+
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    JdbcTemplate SQLDataLoader;
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/SQLDatabaseName");
+        dataSourceBuilder.username("admin");
+        dataSourceBuilder.password("reallystrongpassword");
+        return dataSourceBuilder.build();
+    }
+
+
+    JdbcTemplate SQLDataLoader = new JdbcTemplate(getDataSource());
     ResourceLoader loader;
     private static final Logger logger = LogManager.getLogger();
 
@@ -28,8 +50,40 @@ public class Controller {
         return ("helloWorld");
     }
 
-    public String robots(){
+    @GetMapping("/login")
+    public String loginPage(Model model){
+        model.addAttribute("user", new User());
+        return "login";
+    }
 
+    @PostMapping("/login")
+    public RedirectView loggingIn(@ModelAttribute User user, Model model){
+        model.addAttribute("user", user);
+
+        String sql = "SELECT USER_ID, ENCRYPTED_PASSWORD FROM APP_USER WHERE USER_NAME = " + user.getUsername();
+
+        List<User> r = SQLDataLoader.query(sql, new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User();
+                user.setUsername(rs.getString(1));
+                user.setPassword(rs.getString(2));
+                return user;
+            }
+        });
+
+        if (r.isEmpty())
+            return new RedirectView("/hello");
+        else {
+            if (Base64.getEncoder().encodeToString(user.getPassword().getBytes()).equals(r.get(0).getPassword()))
+                return new RedirectView("/16144cf950518a312e26b9827d91449d166ed6e38e7cc569a9f3c559");
+            else
+                return new RedirectView("/hello");
+        }
+
+    }
+
+    public String robots(){
         Resource resource = loader.getResource("classpath:static/robots.txt");
         return asString(resource);
     }
---
+++ src/main/java/dtu/project/log4jboot2root/User.java
@@ -0,0 +1,23 @@
+package dtu.project.log4jboot2root;
+
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
