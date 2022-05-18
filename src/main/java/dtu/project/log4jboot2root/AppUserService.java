416b54dc3bbe5400350dac36ed980fabbd508625
Holmness <59558117+Holmness@users.noreply.github.com>
2022-05-18 17:55:37 +0200

    Updates frontend

--- src/main/java/dtu/project/log4jboot2root/AppUserService.java
+++ src/main/java/dtu/project/log4jboot2root/AppUserService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class    AppUserService {
+public class AppUserService {
 
     private AppUserDAO appUserDAO = new AppUserDAO();
 
