fde7134d3f0b14faa2c23ce6d3f024ec9d8bbd52
jaholm <jaholm@dtu.dk>
2022-05-31 13:15:27 -0400

    I did an Oof, fixed name change of WEBAPP_NAME to WebApp

---
+++ WebApp/src/main/java/dtu/project/log4jboot2root/Ticket.java
@@ -0,0 +1,31 @@
+package dtu.project.log4jboot2root;
+
+public class Ticket {
+    private String ticketID;
+    private String createdBy;
+    private String description;
+
+    public String getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(String ticketID) {
+        this.ticketID = ticketID;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ WebApp/src/test/java/dtu/project/log4jboot2root/Log4jBoot2rootApplicationTests.java
@@ -0,0 +1,13 @@
+package dtu.project.log4jboot2root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4jBoot2rootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
